1f9e88d1ec119cc1d572e353fb8539bcc99db003
Husain <u1456168@unimail.hud.ac.uk>
2017-01-01 17:49:29 +0000

    Completed EP 8

--- website/resources/views/cards/show.blade.php
+++ website/resources/views/cards/show.blade.php
@@ -3,5 +3,15 @@
 @section('content')
 
   <h1> {{$card->title }}</h1>
+<ul>
+
+
+    @foreach ($card->notes as $note)
+      <li>
+         {{$note->body}}
+      </li>
+
+    @endforeach
+  </ul>
 
 @endsection
