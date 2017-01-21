d43122d673590bf7c1098aced87e61ee536fceb8
Husain <u1456168@unimail.hud.ac.uk>
2017-01-21 14:18:41 +0000

    changed h1 on master and changed new page

--- website/resources/views/newpage.blade.php
+++ website/resources/views/newpage.blade.php
@@ -1,1 +1,2 @@
-New page added to master
+
+<h1>New page added to master</h1>
--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-  <h1> The welcome page goes here</h1>
+  <h1> The picture page goes here</h1>
   <h1> The First commit on Rebasing branch</h1>
   <h1> Another commit on master after rebasing commit</h1>
   <p>
