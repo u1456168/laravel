120ffb87c4a68a0d2cd40890d461635b4fdb19f3
Husain <u1456168@unimail.hud.ac.uk>
2017-01-21 13:45:18 +0000

    dont know

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-  <h1> The welcome page goes here</h1>
+  <h1> I changed the header on the master branch for the rebasing trial.</h1>
   <h1> The First commit on Rebasing branch</h1>
   <h1> Another commit on master after rebasing commit</h1>
 
